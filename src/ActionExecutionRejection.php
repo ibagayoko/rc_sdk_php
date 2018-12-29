69acf5c1a1c7395e452502e3abd5179a2d533417
Ismail Bagayoko <bagayoko.ismail@gmail.com>
2018-12-29 15:49:40 -0400

    add ActionExecution Exception

---
+++ src/ActionExecutionRejection.php
@@ -0,0 +1,23 @@
+<?php 
+namespace IBagayoko;
+
+class ActionExecutionRejection extends Exception 
+{
+    protected $action_name;
+
+    protected $message;
+
+    protected function __construct($action_name, $message=null)
+    {
+        $this->action_name = $action_name;
+        $this->message = $message ? $message : "Custom action '$action_name' rejected execution of"
+                        ;
+    }
+
+    public function __toString()
+   {
+      return $this->message;
+   }
+
+
+}
